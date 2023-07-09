c3a9113e806d3ccfa36c0716cdfb649b84904451
AmanSDE <am.amanshahi@gmail.com>
2023-07-09 19:19:57 +0530

    Second commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+	
+	int n1 = 5;
+	int n2 = 2;
+	
+	public void add() {
+		System.out.println("Sum is = "+(n1+n2));
+	}
+	public static void main(String args[]) {
+		Calculator c = new Calculator();
+		c.add();
+	}
+
+}
