91d716cad3e14badcc6de78b4f4c41471379f395
AmanSDE <am.amanshahi@gmail.com>
2023-07-09 19:25:02 +0530

    Third commit

--- TestProject/src/Calculator.java
+++ TestProject/src/Calculator.java
@@ -7,6 +7,10 @@ public class Calculator {
 	public void add() {
 		System.out.println("Sum is = "+(n1+n2));
 	}
+	
+	public void product() {
+		System.out.println("Product = "+(n1*n2));
+	}
 	public static void main(String args[]) {
 		Calculator c = new Calculator();
 		c.add();
